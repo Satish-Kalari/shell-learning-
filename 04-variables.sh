34a615eb72d2a387db2e56bacf1f27bae45b5366
Satish-Kalari <sarvag@gmail.com>
2023-12-12 18:28:25 +0530

    3rd file

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME1=$1
+NAME2=$2
+
+echo "$NAME1: Hello how are you?"
+echo "$NAME2: I am doing good? How about your self"
+echo "$NAME1: I am good too"
+echo "$NAME1: Ok, see you soon"
