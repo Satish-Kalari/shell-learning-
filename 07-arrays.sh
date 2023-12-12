74e6894816e6f4ed167ae2321697bdef09cec707
Satish-Kalari <sarvag@gmail.com>
2023-12-12 18:57:48 +0530

    3rd file

---
+++ 07-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VEGITABLES=("Tomato" "Potato" "Bringal")
+
+echo "First Vegitable is: ${VEGITABLES[0]}"
+echo "Second Vegitable is: ${VEGITABLES[1]}"
+echo "Thisrd Vegitable is: ${VEGITABLES[2]}"
