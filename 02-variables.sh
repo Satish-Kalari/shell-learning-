76fd8965940e12e8985a8492caab27c3559b67a5
Satish-Kalari <sarvag@gmail.com>
2023-12-12 18:42:05 +0530

    3rd file

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ NAME1=Apeksha
 NAME2=Satish
 
 echo "$NAME1: Hello how are you?"
-echo "S$NAME2: I am doing good? How about your self"
+echo "$NAME2: I am doing good? How about your self"
 echo "$NAME1: I am good too"
 echo "$NAME1: Ok, see you soon"
