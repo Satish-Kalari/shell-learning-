1b82ec07fc5db3926b3c2f6a07dc3a7e6c69c4ed
Satish-Kalari <sarvag@gmail.com>
2023-12-18 16:29:14 +0530

    new file

--- 18-deleting-old-logs.sh
+++ 18-deleting-old-logs.sh
@@ -12,7 +12,7 @@ then
     echo -e "$R Sourec Directory: $SOURCE_DIR Dose Not Exsist $N"
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime 15 -name *.log) # -type f= type to find is file, -mtime=Modified time, 15=days, -name *.log= fild all files with extention .log
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime 15 -name "*.log") # -type f= type to find is file, -mtime=Modified time, 15=days, -name *.log= fild all files with extention .log
 
 while IFS= read -r line #IFS= Interna; Feild Separator
 do
