7077f0a25587a1773c64efeaacd35de8131be6cc
Satish-Kalari <sarvag@gmail.com>
2023-12-18 18:05:40 +0530

    updating scripts

--- 23-delet-archive.sh
+++ 23-delet-archive.sh
@@ -13,7 +13,8 @@ days=""
 max_size=""
 
 # Parse command-line options
-while getopts ":s:a:d:t:m:" opt; do
+while getopts ":s:a:d:t:m:" opt; 
+do
  case ${opt} in
    s )
      source_directory=$OPTARG
@@ -43,31 +44,37 @@ done
 shift $((OPTIND -1))
 
 # Check if action is either delete or archive
-if [ "$action" != "delete" ] && [ "$action" != "archive" ]; then
+if [ "$action" != "delete" ] && [ "$action" != "archive" ]; 
+then
  echo -e "$R Invalid action.$N Action must be either delete or archive."
  exit 1
 fi
 
 # Check if action is archive and destination directory is provided
-if [ "$action" = "archive" ] && [ -z "$destination_directory" ]; then
+if [ "$action" = "archive" ] && [ -z "$destination_directory" ]; 
+then
  echo -e " $R Destination directory must be provided when action is archive.$N"
  exit 1
 fi
 
 # If max_size is provided, find files older than the specified number of days and larger than the maximum size
-if [ -n "$max_size" ]; then
+if [ -n "$max_size" ]; 
+then
  files=$(find "$source_directory" -name "*.log" -mtime +$days -size +$max_size)
 else
  files=$(find "$source_directory" -name "*.log" -mtime +$days)
 fi
 
 # Loop over the files
-for file in $files; do
+for file in $files; 
+do
  # If action is delete, delete the file
- if [ "$action" = "delete" ]; then
+ if [ "$action" = "delete" ]; 
+ then
  rm "$file"
  # If action is archive, archive the file
- elif [ "$action" = "archive" ]; then
+ elif [ "$action" = "archive" ]; 
+ then
  zip -r "$destination_directory/$(basename "$file").zip" "$file"
  rm "$file"
  fi
